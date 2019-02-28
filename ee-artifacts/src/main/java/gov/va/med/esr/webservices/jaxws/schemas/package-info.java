82008467c5eb9e52bb23e1775b69d763f371e9cd
jhulbert <joshua.hulbert@libertyits.com>
2019-02-28 15:06:30 -0500

    Deleted EE things

--- ee-artifacts/src/main/java/gov/va/med/esr/webservices/jaxws/schemas/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://jaxws.webservices.esr.med.va.gov/schemas", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package gov.va.med.esr.webservices.jaxws.schemas;
