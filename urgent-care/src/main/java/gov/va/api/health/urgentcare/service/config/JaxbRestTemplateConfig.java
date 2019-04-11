c152d8127abce54c808adf99cab3a80bdd9ad515
jhulbert <joshua.hulbert@libertyits.com>
2019-04-11 17:42:13 -0400

    Added Xml to RestTemplate instead of overriding

--- urgent-care/src/main/java/gov/va/api/health/urgentcare/service/config/JaxbRestTemplateConfig.java
+++ urgent-care/src/main/java/gov/va/api/health/urgentcare/service/config/JaxbRestTemplateConfig.java
@@ -1,14 +1,22 @@
 package gov.va.api.health.urgentcare.service.config;
 
 import gov.va.api.health.autoconfig.configuration.SecureRestTemplateConfig;
+
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import javax.validation.constraints.Null;
 
 /**
  * Supplier for RestTemplate that support JAXB.
@@ -26,8 +34,7 @@ public class JaxbRestTemplateConfig {
   @WithJaxb
   public RestTemplate jaxbRestTemplate(@Autowired RestTemplateBuilder restTemplateBuilder) {
     RestTemplate restTemplate = secureConfig.restTemplate(restTemplateBuilder);
-    restTemplate.setMessageConverters(
-        Collections.singletonList(new Jaxb2RootElementHttpMessageConverter()));
+    restTemplate.getMessageConverters().add(0, new Jaxb2RootElementHttpMessageConverter());
     return restTemplate;
   }
 }
