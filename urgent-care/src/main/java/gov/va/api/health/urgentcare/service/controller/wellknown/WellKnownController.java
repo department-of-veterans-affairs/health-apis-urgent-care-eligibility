f312e98059db69c76c6308b41a288db3793fee76
jtorano <joseph.torano@libertyits.com>
2019-05-29 13:49:56 -0400

    WellKnown endpoint WIP beginnings

---
+++ urgent-care/src/main/java/gov/va/api/health/urgentcare/service/controller/wellknown/WellKnownController.java
@@ -0,0 +1,31 @@
+package gov.va.api.health.urgentcare.service.controller.wellknown;
+
+import gov.va.api.health.dstu2.api.information.WellKnown;
+import gov.va.api.health.urgentcare.service.controller.conformance.ConformanceStatementProperties;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(
+  value = {".well-known/smart-configuration", "api/.well-known/smart-configuration"},
+  produces = {"application/json", "application/fhir+json", "application/json+fhir"}
+)
+@AllArgsConstructor(onConstructor = @__({@Autowired}))
+class WellKnownController {
+  private final WellKnownProperties wellKnownProperties;
+  private final ConformanceStatementProperties conformanceStatementProperties;
+
+  @GetMapping
+  WellKnown read() {
+    return WellKnown.builder()
+        .authorizationEndpoint(conformanceStatementProperties.getSecurity().getAuthorizeEndpoint())
+        .tokenEndpoint(conformanceStatementProperties.getSecurity().getTokenEndpoint())
+        .capabilities(wellKnownProperties.getCapabilities())
+        .responseTypeSupported(wellKnownProperties.getResponseTypeSupported())
+        .scopesSupported(wellKnownProperties.getScopesSupported())
+        .build();
+  }
+}
