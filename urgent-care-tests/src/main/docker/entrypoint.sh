a03821a5d83c08855cfdf2a0d0ead7844679e79d
jtorano-va <43884659+jtorano-va@users.noreply.github.com>
2019-09-03 16:44:45 -0400

    Task/cer 66 creation of urgent care test suite (#44)
    * Using Lab Robots to do OAuth Login testing
    * Made tests runnable from Docker image

--- urgent-care-tests/src/main/docker/entrypoint.sh
+++ urgent-care-tests/src/main/docker/entrypoint.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
-ENDPOINT_DOMAIN_NAME="$K8S_LOAD_BALANCER"
+set -o pipefail
+
+#For running our test suite (currently for lab validation/stress testing only)
+[ -z "$TEST_BASE_DIR" ] && TEST_BASE_DIR=/test-suite
+cd $TEST_BASE_DIR
+MAIN_JAR=$(find -maxdepth 1 -name "urgent-care-tests-*.jar" -a -not -name "urgent-care-tests-*-tests.jar")
+TESTS_JAR=$(find -maxdepth 1 -name "urgent-care-tests-*-tests.jar")
+WEB_DRIVER_PROPERTIES="-Dwebdriver.chrome.driver=/usr/local/bin/chromedriver -Dwebdriver.chrome.headless=true"
+SYSTEM_PROPERTIES=$WEB_DRIVER_PROPERTIES
+EXCLUDE_CATEGORY=
+INCLUDE_CATEGORY=
+
+
+#For doing our simplistic smoke and regression tests
 ENVIRONMENT="$K8S_ENVIRONMENT"
 TOKEN="$TOKEN"
 PATIENT="$PATIENT"
@@ -11,11 +24,34 @@ PATHS=(/fhir/v0/r4/metadata \
 SUCCESS=0
 FAILURE=0
 
+
+# Command to make LAB OAuth Test work
+# shm-size needs to be increased to 1g.
+# shm is just shared temporary file storage on Unix.  I don't know why this script runs out while others dont...
+
+# docker run --rm --init --shm-size="1g" --network=host
+#     --env K8S_ENVIRONMENT=lab
+#     --env-file staging.testvars
+#     vasdvp/health-apis-urgent-care-tests:latest ol
+
+# OR
+
+# docker run --rm --init --shm-size="1g" --network=host
+#     --env-file staging.testvars
+#     vasdvp/health-apis-urgent-care-tests:latest ol
+#     --environment=lab
+
+# The first one matches more in line of how DQ does it.  The second option matches how our regression/smoke tests work.
+
 usage() {
   cat <<EOF
   Commands
+    list-tests
+    list-categories
+    test [--include-category <category>] [--exclude-category <category>] [-Dkey=value] <name> [name] [...]
     smoke-test [--endpoint-domain-name|-d <endpoint>] [--environment|-e <env>] [--token|-t <token>] [--patient|-p <ICN>]
     regression-test [--endpoint-domain-name|-d <endpoint>] [--environment|-e <env>] [--token|-t <token>] [--patient|-p <ICN>]
+    oauth-lab-test [--environment|-e <env>]
 
   Example
     smoke-test
@@ -30,6 +66,84 @@ EOF
 exit 1
 }
 
+#### Start of test suite runner commands ####
+defaultTest() {
+  # Our only test is for Oauth right now. We can default to integration tests whenever we have some
+  doListTests | grep 'Oauth'
+}
+
+# This feels like overkill for our single test, but ignoring all this output feels more important as our suite grows
+doTest() {
+  local tests="$@"
+  [ -z "$tests" ] && tests=$(defaultTest)
+  local filter
+  [ -n "$EXCLUDE_CATEGORY" ] && filter+=" --filter=org.junit.experimental.categories.ExcludeCategories=$EXCLUDE_CATEGORY"
+  [ -n "$INCLUDE_CATEGORY" ] && filter+=" --filter=org.junit.experimental.categories.IncludeCategories=$INCLUDE_CATEGORY"
+  local noise="org.junit"
+  noise+="|groovy.lang.Meta"
+  noise+="|io.restassured.filter"
+  noise+="|io.restassured.internal"
+  noise+="|java.lang.reflect"
+  noise+="|java.net"
+  noise+="|org.apache.http"
+  noise+="|org.codehaus.groovy"
+  noise+="|sun.reflect"
+  java -cp "$(pwd)/*" $SYSTEM_PROPERTIES org.junit.runner.JUnitCore $filter $tests \
+    | grep -vE "^	at ($noise)"
+
+  # Exit on failure otherwise let other actions run.
+  [ $? != 0 ] && exit 1
+}
+
+doListTests() {
+  jar -tf $TESTS_JAR \
+    | grep -E '(IT|Test)\.class' \
+    | sed 's/\.class//' \
+    | tr / . \
+    | sort
+}
+
+doListCategories() {
+  # There are no urgent care specific categories if you're comparing this to dq
+  jar -tf $MAIN_JAR \
+    | grep -E 'gov/va/api/health/sentinel/categories/.*\.class' \
+    | sed 's/\.class//' \
+    | tr / . \
+    | sort
+}
+
+
+
+checkVariablesForAutomation() {
+  # Check out required deployment variables and urgent care specific variables.
+  for param in "USER_PASSWORD" "CLIENT_ID" "CLIENT_SECRET" \
+    "REDIRECT_URL" "AUD" "STATE" "BASE_URL" "REQUEST_MODE"; do
+    [ -z ${!param} ] && usage "Variable $param must be specified."
+  done
+}
+
+setupForAutomation() {
+  checkVariablesForAutomation
+
+  SYSTEM_PROPERTIES="$WEB_DRIVER_PROPERTIES \
+    -D${ENVIRONMENT}.user-password=$USER_PASSWORD \
+    -D${ENVIRONMENT}.client-id=$CLIENT_ID \
+    -D${ENVIRONMENT}.client-secret=$CLIENT_SECRET \
+    -D${ENVIRONMENT}.redirect-url=$REDIRECT_URL \
+    -D${ENVIRONMENT}.aud=$AUD \
+    -D${ENVIRONMENT}.state=$STATE \
+    -D${ENVIRONMENT}.base-url=$BASE_URL \
+    -D${ENVIRONMENT}.credentials-mode=$REQUEST_MODE"
+}
+
+doLabOauthTest(){
+  setupForAutomation
+
+  INCLUDE_CATEGORY=gov.va.api.health.sentinel.categories.Manual
+  doTest
+}
+
+#### Start of smoke and regression tests ####
 doCurl () {
   REQUEST_URL="$ENDPOINT_DOMAIN_NAME$path"
   if [[ -n "$2" ]]
@@ -50,6 +164,22 @@ doCurl () {
 }
 
 doSmokeTest () {
+  if [[ -z "$ENDPOINT_DOMAIN_NAME" || -e "$ENDPOINT_DOMAIN_NAME" ]]; then
+    usage "Missing variable K8S_LOAD_BALANCER or option --endpoint-domain-name|-d."
+  fi
+
+  if [[ -z "$ENVIRONMENT" || -e "$ENVIRONMENT" ]]; then
+    usage "Missing variable K8S_ENVIRONMENT or option --environment|-e."
+  fi
+
+  if [[ -z "$TOKEN" || -e "$TOKEN" ]]; then
+    usage "Missing variable TOKEN or option --token|-t."
+  fi
+
+  if [[ -z "$PATIENT" || -e "$PATIENT" ]]; then
+    usage "Missing variable PATIENT or option --patient|-p."
+  fi
+
   if [[ ! "$ENDPOINT_DOMAIN_NAME" == http* ]]; then
     ENDPOINT_DOMAIN_NAME="https://$ENDPOINT_DOMAIN_NAME"
   fi
@@ -80,8 +210,8 @@ doRegressionTest() {
 }
 
 ARGS=$(getopt -n $(basename ${0}) \
-    -l "endpoint-domain-name:,environment:,token:,patient:,help" \
-    -o "d:e:t:p:h" -- "$@")
+    -l "endpoint-domain-name:,environment:,token:,patient:,exclude-category:,include-category:,help" \
+    -o "d:e:t:p:hD:x:i:" -- "$@")
 [ $? != 0 ] && usage
 eval set -- "$ARGS"
 while true
@@ -89,8 +219,11 @@ do
   case "$1" in
     -d|--endpoint-domain-name) ENDPOINT_DOMAIN_NAME=$2;;
     -e|--environment) ENVIRONMENT=$2;;
+    -D) SYSTEM_PROPERTIES+=" -D$2";;
     -t|--token) TOKEN=$2;;
     -p|--patient) PATIENT=$2;;
+    -x|--exclude-category) EXCLUDE_CATEGORY=$2;;
+    -i|--include-category) INCLUDE_CATEGORY=$2;;
     -h|--help) usage '
  __________________
  |< Hey! Listen! >|
@@ -109,29 +242,17 @@ do
   shift;
 done
 
-if [[ -z "$ENDPOINT_DOMAIN_NAME" || -e "$ENDPOINT_DOMAIN_NAME" ]]; then
-  usage "Missing variable K8S_LOAD_BALANCER or option --endpoint-domain-name|-d."
-fi
-
-if [[ -z "$ENVIRONMENT" || -e "$ENVIRONMENT" ]]; then
-  usage "Missing variable K8S_ENVIRONMENT or option --environment|-e."
-fi
-
-if [[ -z "$TOKEN" || -e "$TOKEN" ]]; then
-  usage "Missing variable TOKEN or option --token|-t."
-fi
-
-if [[ -z "$PATIENT" || -e "$PATIENT" ]]; then
-  usage "Missing variable PATIENT or option --patient|-p."
-fi
-
 [ $# == 0 ] && usage "No command specified"
 COMMAND=$1
 shift
 
 case "$COMMAND" in
+  t|test) doTest $@;;
+  lc|list-categories) doListCategories;;
+  lt|list-tests) doListTests;;
   s|smoke-test) doSmokeTest;;
   r|regression-test) doRegressionTest;;
+  ol|oauth-lab-test) doLabOauthTest;;
   *) usage "Unknown command: $COMMAND";;
 esac
 
---
+++ urgent-care-tests/src/test/java/gov/va/api/health/urgentcare/tests/OauthLoginTest.java
@@ -0,0 +1,80 @@
+package gov.va.api.health.urgentcare.tests;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import gov.va.api.health.sentinel.LabBot;
+import gov.va.api.health.sentinel.LabBot.LabBotUserResult;
+import gov.va.api.health.sentinel.categories.Manual;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Slf4j
+public class OauthLoginTest {
+
+  private static List<String> urgentCareScopes() {
+    return Arrays.asList(
+        "patient/CoverageEligibilityResponse.read", "offline_access", "launch/patient");
+  }
+
+  @Test
+  @SneakyThrows
+  @Category(Manual.class)
+  public void RequestTest() {
+    List<LabBotUserResult> labBotUserResultList =
+        LabBot.builder()
+            .userIds(LabBot.allUsers())
+            .scopes(urgentCareScopes())
+            .configFile("config/lab.properties")
+            .build()
+            .request("/services/fhir/v0/r4/CoverageEligibilityResponse?patient={icn}");
+    List<String> winners = new ArrayList<>();
+    List<String> losers = new ArrayList<>();
+    log.info("Checking that the number of lab bot users is not 0");
+    assertThat(labBotUserResultList.size()).isNotZero();
+    for (LabBotUserResult labBotUserResult : labBotUserResultList) {
+      if (!labBotUserResult.tokenExchange().isError()
+          && labBotUserResult
+              .response()
+              .contains("\"resourceType\":\"CoverageEligibilityResponse\"")) {
+        log.info(
+            "Winner: {} is patient {}.",
+            labBotUserResult.user().id(),
+            labBotUserResult.tokenExchange().patient());
+        winners.add(
+            labBotUserResult.user().id()
+                + " is patient "
+                + labBotUserResult.tokenExchange().patient());
+      } else {
+        log.info(
+            "Loser: {} is patient {}.",
+            labBotUserResult.user().id(),
+            labBotUserResult.tokenExchange().patient());
+        losers.add(
+            labBotUserResult.user().id()
+                + " is patient "
+                + labBotUserResult.tokenExchange().patient()
+                + " - "
+                + labBotUserResult.tokenExchange().error()
+                + ": "
+                + labBotUserResult.tokenExchange().errorDescription());
+      }
+    }
+    String report =
+        Stream.concat(winners.stream().map(w -> w + " - OK"), losers.stream())
+            .sorted()
+            .collect(Collectors.joining("\n"));
+    Files.write(new File("lab-users.txt").toPath(), report.getBytes(StandardCharsets.UTF_8));
+    log.info("Lab Users:\n{}", report);
+    assertThat(losers.size()).isZero();
+  }
+}
