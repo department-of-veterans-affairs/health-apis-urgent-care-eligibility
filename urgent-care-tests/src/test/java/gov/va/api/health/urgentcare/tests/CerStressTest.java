02eb4fcb22bbe735676011da3c87176ad0e18d1b
Beau Grantham <beau@nologs.org>
2019-09-23 10:00:42 -0400

    Extract bot for hopeful reuse; refactor to make it more usable

--- urgent-care-tests/src/test/java/gov/va/api/health/urgentcare/tests/CerStressTest.java
+++ urgent-care-tests/src/test/java/gov/va/api/health/urgentcare/tests/CerStressTest.java
@@ -2,10 +2,6 @@ package gov.va.api.health.urgentcare.tests;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import gov.va.api.health.sentinel.LabBot;
-import gov.va.api.health.sentinel.LabBot.LabBotUserResult;
-import gov.va.api.health.sentinel.LabBot.LabBotUserResult.LabBotUserResultBuilder;
-import gov.va.api.health.sentinel.categories.Manual;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -14,20 +10,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
-import lombok.Builder;
-import lombok.NonNull;
-import lombok.SneakyThrows;
-import lombok.extern.slf4j.Slf4j;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+
+import gov.va.api.health.sentinel.LabBot.LabBotUserResult;
+import gov.va.api.health.sentinel.categories.Manual;
+import gov.va.api.health.urgentcare.tests.StressBot.StressBotRequest;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CerStressTest {
@@ -42,6 +34,11 @@ public class CerStressTest {
 
   // Maximum test duration (in minutes) (-Dmaximum-runtime=10)
   private static int maximumRuntime = 10;
+
+  private static final List<String> userIds = Arrays.asList("va.api.user+idme.123@gmail.com");
+
+  private static final String url =
+      "/services/fhir/v0/r4/CoverageEligibilityResponse?patient={icn}";
 
   @BeforeClass
   public static void setup() {
@@ -78,23 +75,26 @@ public class CerStressTest {
   @Category(Manual.class)
   public void stressTest() {
     log.info(
-        "Begin stress test [{} request, {} concurrent, {} minute(s) maximum runtime]",
+        "Begin stress test [{} concurrent, {} minute(s) maximum runtime]",
         requests,
         concurrentRequests,
         maximumRuntime);
 
     StressBot stressBot =
         StressBot.builder()
-            .user("va.api.user+idme.123@gmail.com")
-            .scopes(urgentCareScopes())
-            .configFile("config/lab.properties")
-            .requests(requests)
-            .concurrentRequests(concurrentRequests)
             .maximumRuntime(maximumRuntime)
+            .concurrentRequests(concurrentRequests)
+            .configFile("config/lab.properties")
             .build();
 
     List<LabBotUserResult> results =
-        stressBot.request("/services/fhir/v0/r4/CoverageEligibilityResponse?patient={icn}");
+        stressBot.request(
+            StressBotRequest.builder()
+                .url(url)
+                .times(requests)
+                .userIds(userIds)
+                .scopes(urgentCareScopes())
+                .build());
 
     assertThat(results.size()).withFailMessage("No results").isGreaterThan(0);
 
@@ -113,7 +113,7 @@ public class CerStressTest {
     StringBuilder output =
         new StringBuilder()
             .append("Requests desired   : ")
-            .append(requests)
+            .append(requests * userIds.size())
             .append(System.lineSeparator())
             .append("Requests attempted : ")
             .append(results.size())
@@ -136,141 +136,5 @@ public class CerStressTest {
     }
 
     assertThat(failures.size()).isZero();
-  }
-
-  /** Bot with functionality for repetitive requests (e.g. load/stress testing) */
-  @Slf4j
-  static class StressBot {
-
-    private LabBot labBot;
-
-    private int requests;
-
-    private int concurrentRequests;
-
-    private int maximumRuntime;
-
-    /**
-     * Constructs a new stress bot.
-     *
-     * @param user The user for executing requests.
-     * @param scopes The scopes that for token requests.
-     * @param configFile The configuration file.
-     * @param requests The number of requests to execute.
-     * @param concurrentRequests The number of concurrent requests / threads.
-     * @param maximumRuntime The maximum runtime in minutes for all requests to finish.
-     */
-    @Builder
-    public StressBot(
-        @NonNull String user,
-        @NonNull List<String> scopes,
-        @NonNull String configFile,
-        int requests,
-        int concurrentRequests,
-        int maximumRuntime) {
-      labBot =
-          LabBot.builder()
-              .userIds(Arrays.asList(user))
-              .scopes(scopes)
-              .configFile(configFile)
-              .build();
-
-      this.requests = requests;
-      this.concurrentRequests = concurrentRequests;
-      this.maximumRuntime = maximumRuntime;
-    }
-
-    private LabBotUserResult authenticate() {
-      List<LabBotUserResult> tokens = labBot.tokens();
-
-      if (tokens.size() != 1) {
-        throw new AssertionError("Token exchange not successful");
-      }
-
-      return tokens.get(0);
-    }
-
-    private void awaitResults(ExecutorService ex, List<Future<?>> futures)
-        throws InterruptedException {
-      ex.shutdown();
-
-      if (!ex.awaitTermination(maximumRuntime, TimeUnit.MINUTES)) {
-        log.info("Maximum runtime has been exceeded; cancelling remaining tasks");
-      }
-
-      /*
-       * Proceed when all tasks are complete or when X minutes have been exceeded.
-       *
-       * Attempt to cancel any remaining tasks and fail the test if any unexpected
-       * exceptions are thrown.
-       */
-
-      int errors =
-          futures
-              .stream()
-              .mapToInt(
-                  f -> {
-                    try {
-                      if (!f.isDone()) {
-                        f.cancel(true);
-                      }
-                      f.get();
-                      return 0;
-                    } catch (CancellationException e) {
-                      return 0;
-                    } catch (Exception e) {
-                      log.error(e.getMessage());
-                      return 1;
-                    }
-                  })
-              .sum();
-
-      if (errors > 0) {
-        throw new AssertionError("Thread processing had " + errors + " unexpected errors");
-      }
-    }
-
-    /**
-     * Given a path send a request for each user. Replaces {icn} with patient icn to send request.
-     * Useful for when you want to send a given request to a set of users.
-     *
-     * @param path The path to use for the requests, must contain {icn} to be replaced with the
-     *     patient icn.
-     * @return a list of results.
-     */
-    @SneakyThrows
-    public List<LabBotUserResult> request(String path) {
-      LabBotUserResult token = authenticate();
-
-      List<LabBotUserResult> results = new CopyOnWriteArrayList<>();
-      List<Future<?>> futures = new ArrayList<>(requests);
-      ExecutorService ex = Executors.newFixedThreadPool(concurrentRequests);
-
-      for (int i = 0; i < requests; i++) {
-        futures.add(
-            ex.submit(
-                () -> {
-                  LabBotUserResultBuilder result =
-                      LabBotUserResult.builder()
-                          .user(token.user())
-                          .tokenExchange(token.tokenExchange());
-                  try {
-                    result.response(
-                        labBot.request(
-                            path.replace("{icn}", token.tokenExchange().patient()),
-                            token.tokenExchange().accessToken()));
-                  } catch (Exception e) {
-                    log.error("Request failure: {}", e.getMessage(), e.getCause());
-                    result.response(e.getClass().getName() + ": " + e.getMessage());
-                  } finally {
-                    results.add(result.build());
-                  }
-                }));
-      }
-
-      awaitResults(ex, futures);
-
-      return results;
-    }
   }
 }
