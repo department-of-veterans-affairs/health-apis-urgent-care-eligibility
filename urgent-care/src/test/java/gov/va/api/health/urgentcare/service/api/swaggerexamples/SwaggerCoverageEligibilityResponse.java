f6352fe411f36bed5b21e2652fd47dc9a7c7cc69
Lesley Brown <lesley.brown@libertyits.com>
2019-10-08 14:30:41 -0400

    Add missing example.

--- urgent-care/src/test/java/gov/va/api/health/urgentcare/service/api/swaggerexamples/SwaggerCoverageEligibilityResponse.java
+++ urgent-care/src/test/java/gov/va/api/health/urgentcare/service/api/swaggerexamples/SwaggerCoverageEligibilityResponse.java
@@ -24,6 +24,48 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class SwaggerCoverageEligibilityResponse {
+
+  static final CoverageEligibilityResponse SWAGGER_EXAMPLE_COVERAGEELIGIBILITYRESPONSE =
+      CoverageEligibilityResponse.builder()
+          .id("1008679665V880686")
+          .resourceType("CoverageEligibilityResponse")
+          .identifier(
+              singletonList(
+                  Identifier.builder()
+                      .system("http://www.va.gov/FHIR/R4/coverageeligibilityresponse")
+                      .value("1008679665V880686")
+                      .build()))
+          .status(Status.active)
+          .purpose(singletonList(Purpose.discovery))
+          .patient(Reference.builder().display("Patient/1008679665V880686").build())
+          .created("2019-02-21T23:44:32.000-06:00")
+          .request(null)
+          ._request(DataAbsentReason.of(DataAbsentReason.Reason.unsupported))
+          .outcome(Outcome.complete)
+          .insurer(Reference.builder().display("Veterans Administration").build())
+          .insurance(
+              singletonList(
+                  Insurance.builder()
+                      .coverage(null)
+                      ._coverage(DataAbsentReason.of(DataAbsentReason.Reason.unsupported))
+                      .benefitPeriod(
+                          Period.builder().start("2019-02-21T23:44:32.000-06:00").build())
+                      .item(
+                          singletonList(
+                              Item.builder()
+                                  .category(
+                                      CodeableConcept.builder()
+                                          .coding(
+                                              singletonList(
+                                                  Coding.builder()
+                                                      .code("U")
+                                                      .display("Urgent Care")
+                                                      .build()))
+                                          .build())
+                                  .build()))
+                      .build()))
+          .build();
+
   static final Bundle SWAGGER_EXAMPLE_COVERAGEELIGIBILITYRESPONSE_BUNDLE =
       Bundle.builder()
           .resourceType("Bundle")
@@ -47,55 +89,6 @@ public class SwaggerCoverageEligibilityResponse {
                           "https://dev-api.va.gov/services/fhir/v0/r4/CoverageEligibilityResponse?patient=1008679665V880686&page=1&_count=15")
                       .build()))
           .entry(
-              asList(
-                  Entry.builder()
-                      .resource(
-                          CoverageEligibilityResponse.builder()
-                              .id("1008679665V880686")
-                              .resourceType("CoverageEligibilityResponse")
-                              .identifier(
-                                  singletonList(
-                                      Identifier.builder()
-                                          .system(
-                                              "http://www.va.gov/FHIR/R4/coverageeligibilityresponse")
-                                          .value("1008679665V880686")
-                                          .build()))
-                              .status(Status.active)
-                              .purpose(singletonList(Purpose.discovery))
-                              .patient(
-                                  Reference.builder().display("Patient/1008679665V880686").build())
-                              .created("2019-02-21T23:44:32.000-06:00")
-                              .request(null)
-                              ._request(DataAbsentReason.of(DataAbsentReason.Reason.unsupported))
-                              .outcome(Outcome.complete)
-                              .insurer(
-                                  Reference.builder().display("Veterans Administration").build())
-                              .insurance(
-                                  singletonList(
-                                      Insurance.builder()
-                                          .coverage(null)
-                                          ._coverage(
-                                              DataAbsentReason.of(
-                                                  DataAbsentReason.Reason.unsupported))
-                                          .benefitPeriod(
-                                              Period.builder()
-                                                  .start("2019-02-21T23:44:32.000-06:00")
-                                                  .build())
-                                          .item(
-                                              singletonList(
-                                                  Item.builder()
-                                                      .category(
-                                                          CodeableConcept.builder()
-                                                              .coding(
-                                                                  singletonList(
-                                                                      Coding.builder()
-                                                                          .code("U")
-                                                                          .display("Urgent Care")
-                                                                          .build()))
-                                                              .build())
-                                                      .build()))
-                                          .build()))
-                              .build())
-                      .build()))
+              asList(Entry.builder().resource(SWAGGER_EXAMPLE_COVERAGEELIGIBILITYRESPONSE).build()))
           .build();
 }
