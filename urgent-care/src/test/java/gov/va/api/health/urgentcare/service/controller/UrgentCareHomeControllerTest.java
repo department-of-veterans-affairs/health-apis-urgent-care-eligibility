1fb2c005f326cfdc54a82b23bfea414e8048be2d
Lesley Brown <lesley.brown@libertyits.com>
2019-10-14 15:39:01 -0400

    Move properties to dev profile.

--- urgent-care/src/test/java/gov/va/api/health/urgentcare/service/controller/UrgentCareHomeControllerTest.java
+++ urgent-care/src/test/java/gov/va/api/health/urgentcare/service/controller/UrgentCareHomeControllerTest.java
@@ -12,11 +12,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.StreamUtils;
 
 @RunWith(SpringRunner.class)
+@ActiveProfiles("dev")
 @WebMvcTest(controllers = {UrgentCareHomeController.class})
 public class UrgentCareHomeControllerTest {
 
