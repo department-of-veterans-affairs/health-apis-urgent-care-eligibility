587b596f437ad455b4a43c866763860953c94dbc
jblefkove-va <46896876+jblefkove-va@users.noreply.github.com>
2020-02-26 14:17:04 -0500

    SHANK-345 Remove steel-thread health checks (#61)

--- urgent-care/src/main/java/gov/va/api/health/urgentcare/service/Application.java
+++ urgent-care/src/main/java/gov/va/api/health/urgentcare/service/Application.java
@@ -4,11 +4,9 @@ import gov.va.api.health.queenelizabeth.ee.impl.QueenElizabethConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SuppressWarnings("WeakerAccess")
 @SpringBootApplication
-@EnableScheduling
+@SuppressWarnings("WeakerAccess")
 @Import({QueenElizabethConfig.class})
 public class Application {
   public static void main(String[] args) {
